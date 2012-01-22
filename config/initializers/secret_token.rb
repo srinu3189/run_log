ece04c7f1613c13a344fca5b97ebd21510c285d6
Chuck Riley <charlesriley1@gmail.com>
2012-01-21 22:44:26 -0500

    Flattened top level of directory tree for github cleanliness

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RunLog::Application.config.secret_token = 'b5524fadfcf958c5248a83dd1cc7f71bb3968ecf64637834e61e4f5ebb372b4b602a46002b5253b17a7ee8ff2e1846808a08a5d0d6aa9b7896a8b06862f112cc'
