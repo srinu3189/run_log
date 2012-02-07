d069add89aa0efa38951b1aceba92480d8add31e
Chuck Riley <charlesriley1@gmail.com>
2012-02-06 22:03:58 -0500

    Fixed some hardcoding in the step file.

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -11,8 +11,8 @@ When /^I visit the logbook$/ do
 	visit log_book_path
 end
 
-Then /^I should see a run with (\d+) miles$/ do |arg1|
-	page.should have_content("11")
+Then /^I should see a run with (\d+) miles$/ do |miles|
+	page.should have_content(miles)
 end
 
 Then /^I should see a run with a time of (\d+):(\d+):(\d+)$/ do |hours, minutes, seconds|
