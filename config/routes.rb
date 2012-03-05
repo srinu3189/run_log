2253c650abaabe5d6529c147ae50d45dad20f042
Chuck Riley <charlesriley1@gmail.com>
2012-03-04 22:45:11 -0500

    Cleaning up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ RunLog::Application.routes.draw do
   match 'log_book/new_run' => 'log_book#new_run'
   match 'log_book/destroy_run' => 'log_book#destroy_run'
   match 'log_book/export' => 'log_book#export'
-
+  root :to => 'log_book#index'
 end
