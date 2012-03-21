532a1664f9a84920dec4d4b005a3f13f68d9e927
Chuck Riley <charlesriley1@gmail.com>
2012-03-20 20:22:09 -0400

    Tinkering around with controller spec approaches.  Still needs a lot of work.  Also, the controller is too fat.

--- spec/controllers/log_book_controller_spec.rb
+++ spec/controllers/log_book_controller_spec.rb
@@ -1,9 +1,44 @@
 require 'spec_helper'
 
 describe LogBookController do
-  describe "POST index" do
-    it "lists all of the runs" do
-      #needs some work
+  before (:each) do
+    @runner = FactoryGirl.create(:runner)
+    sign_in @runner
+  end
+
+  describe "#index" do
+    it "returns the list of runs" do
+      run = mock_model(Run);
+      Run.should_receive(:find_all_and_calculate_mileage).and_return([run])
+      get 'index' 
+      assigns[:runs].should == [run]
     end
   end	
+
+  describe "#new_run" do
+    it "saves a run" do
+      run = mock_model(Run)
+      Run.should_receive(:new).and_return(run)
+      run.should_receive(:save)
+      post 'new_run', :run => { :date => Date.today, :miles => 15 } 
+    end
+  end
+
+  describe "#destroy_run" do
+    it "destroys a run" do
+      run = mock_model(Run)
+      Run.should_receive(:find).with('9').and_return(run)
+      run.should_receive(:destroy)
+      delete 'destroy_run', :id => 9
+    end
+  end
+
+  describe "#export" do
+    it "should find all runs and convert them to csv" do
+      runs = []
+      Run.should_receive(:find_all_and_calculate_mileage).and_return(runs)
+      ExportsRuns.should_receive(:to_csv).with(runs)
+      get 'export'
+    end
+  end
 end
